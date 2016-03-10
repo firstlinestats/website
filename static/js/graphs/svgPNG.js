0ee940ad37d9e7acc98e06034c03508daa9a5636
Matt Daye <mdaye13@gmail.com>
2016-03-09 21:25:15 -0500

    testing a fix for svg resizing

--- static/js/graphs/svgPNG.js
+++ static/js/graphs/svgPNG.js
@@ -1,7 +1,14 @@
 function savePNG(button_id, svg_id) {
   d3.select(button_id).on("click", function(){
     var svgText = $(svg_id).html();
-    console.log(svg_id, svgText);
+    var width = $(svg_id).width();
+    if (width < 1140) {
+        var height = $(svg_id).height();
+        var multiple = 1140 / width;
+        var newwidth = width * multiple;
+        var newheight = height * multiple;
+        svgText = svgText.replace("width=\"" + width + "\"", "width=\"" + newwidth + "\"").replace("height=\"" + height + "\"", "height=\"" + newheight + "\"")
+    }
     canvg('canvas', svgText, { renderCallback: function () {
       var img = canvas.toDataURL("image/png");
       window.open(img);
